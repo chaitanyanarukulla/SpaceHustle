a23dd053b0ee937c256bd3930e7945a21947f6be
Chai.Narukulla <chaitanyanarukulla@gmail.com>
2019-04-15 13:59:04 -0700

    updated user

--- js/app.js
+++ js/app.js
@@ -1,21 +1,24 @@
 'use strict'
-var userName = document.getElementById("userName").value;
-var diffculity = document.getElementById("diffculity").value;
-var shipName = document.getElementById("shipName").value;
-var proPic = document.getElementById("proPic").value;
+var user = {};
+function subbmitUserinfo() {
+    event.preventDefault();
+    var userName = document.getElementById('userName').value;
+    var diffculity = document.getElementById('diffulity').value;
+    var shipName = document.getElementById('shipName').value;
+    var proPic = document.getElementById('proPic').value;
+    user = new User(userName,diffculity,shipName,proPic);
 
+  }
 
-proPic = [pic1,pic2,pic3];
-
-function User(userName,diffculity,shipName,proPic) {
+    var User = function(userName,diffculity,shipName,proPic) {
     this.userName = userName;
     this.diffculity = diffculity;
-    this.shipname = shipName;
+    this.shipName = shipName;
     this.proPic = proPic;
-    currentSector = 1 ;
-    visitedSectorsArray = [];
-    currentHolds = 20;
-    turnsleft = 10; 
-    credits = 100 ;
-    maxHolds = 500;
+    this.currentSector = 1 ;
+    this.visitedSectorsArray = [];
+    this.currentHolds = 20;
+    this.turnsleft = 10; 
+    this.credits = 100 ;
+    this.maxHolds = 500;
 }
